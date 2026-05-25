b4be3f8118ee1a26969841e9033e7b2f89345a32
Tristan Languebien <tlanguebien@gmail.com>
2026-05-25 11:29:59 +0200

    fix typo

--- src/spicy_qc/__init__.py
+++ src/spicy_qc/__init__.py
@@ -1,5 +1,5 @@
 from spicy_qc.api import Criterion, Tag
-from spicy_qc.gui.utils import get_qt_app, get_spyciqc_icon
+from spicy_qc.gui.utils import get_qt_app, get_spicyqc_icon
 from spicy_qc.widgets.container import ContainerDialog, ContainerWidget
 from spicy_qc.widgets.spicyqc_widget import SpicyQcWidget
 
@@ -7,7 +7,7 @@ from spicy_qc.widgets.spicyqc_widget import SpicyQcWidget
 def show_spicyqc_dialog(criterions: list[Criterion], tags: list[Tag]) -> None:
     app = get_qt_app()  # noqa: F841
     widget = SpicyQcWidget(criterions, tags)
-    container = ContainerWidget(widget, title="SpicyQC", icon=get_spyciqc_icon())
+    container = ContainerWidget(widget, title="SpicyQC", icon=get_spicyqc_icon())
     dialog = ContainerDialog(container)
     dialog.exec()
 
